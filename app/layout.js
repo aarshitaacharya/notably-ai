a2abb3f67ab7bffac02474e3cd9e18172a74c181
aarshitaacharya <acharyaaarshita@gmail.com>
2025-06-04 15:51:07 -0500

    authentication using clerk

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono, IBM_Plex_Sans } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 
 const ibmPlexSans = IBM_Plex_Sans({
@@ -25,6 +26,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
       <body
         className={`${ibmPlexSans.variable} antialiased`}
@@ -32,5 +34,6 @@ export default function RootLayout({ children }) {
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
